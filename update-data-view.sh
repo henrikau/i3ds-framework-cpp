5b7472b191c0e461a4bedffc2204ba67da0e6c93
Kristoffer Nyborg Gregertsen <kristoffer.gregertsen@sintef.no>
2017-11-23 11:52:44 +0100

    Removes import of unused base-types

--- update-data-view.sh
+++ update-data-view.sh
@@ -1,8 +1,15 @@
 #! /bin/dash
 
 ROOT=$(pwd)
-BASE=$(ls esrocos/types-base/asn/*.asn)
 
-export FILES="$BASE SensorSuite.asn"
+BASE="$ROOT/esrocos/types-base/asn/Eigen.asn \
+    $ROOT/esrocos/types-base/asn/Point.asn \
+    $ROOT/esrocos/types-base/asn/taste-extended.asn \
+    $ROOT/esrocos/types-base/asn/taste-types.asn \
+    $ROOT/esrocos/types-base/asn/Temperature.asn \
+    $ROOT/esrocos/types-base/asn/Time.asn"
+
+export FILES="$BASE \
+$ROOT/SensorSuite.asn"
 
 taste-update-data-view $FILES
