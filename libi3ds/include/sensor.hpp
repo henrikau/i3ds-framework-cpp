992441c1644bc23854dfa7681301c3aad3ab6a09
Kristoffer Nyborg Gregertsen <kristoffer.gregertsen@sintef.no>
2018-03-14 08:54:31 +0100

    Fixes indention of sensor.hpp

--- libi3ds/include/sensor.hpp
+++ libi3ds/include/sensor.hpp
@@ -17,87 +17,88 @@
 
 namespace i3ds
 {
-  CODEC(SensorStatus);
-  CODEC(SensorCommand);
-  CODEC(SensorConfiguration);
-  CODEC(SensorCommandResponse);
 
-  class Sensor
-  {
-  public:
+CODEC(SensorStatus);
+CODEC(SensorCommand);
+CODEC(SensorConfiguration);
+CODEC(SensorCommandResponse);
 
-    static const EndpointID COMMAND;
-    static const EndpointID STATUS;
-    static const EndpointID CONFIGURATION;
-    static const EndpointID MEASUREMENT;
+class Sensor
+{
+public:
 
-    Sensor(SensorID id);
-    virtual ~Sensor();
+  static const EndpointID COMMAND;
+  static const EndpointID STATUS;
+  static const EndpointID CONFIGURATION;
+  static const EndpointID MEASUREMENT;
 
-    // Get sensor ID.
-    SensorID get_id() const {return id_;}
+  Sensor(SensorID id);
+  virtual ~Sensor();
 
-    // Get sensor state.
-    SensorState get_state() const {return state_;}
+  // Get sensor ID.
+  SensorID get_id() const {return id_;}
 
-    // Get sensor rate.
-    SensorRate get_rate() const {return rate_;}
+  // Get sensor state.
+  SensorState get_state() const {return state_;}
 
-    // Get temperature in Kelvin (defaults to 0.0).
-    virtual double get_temperature() const {return 0.0;}
+  // Get sensor rate.
+  SensorRate get_rate() const {return rate_;}
 
-    // Callback for handling sensor request message.
-    virtual Message handle_request(const Message& message);
+  // Get temperature in Kelvin (defaults to 0.0).
+  virtual double get_temperature() const {return 0.0;}
 
-  protected:
+  // Callback for handling sensor request message.
+  virtual Message handle_request(const Message& message);
 
-    // Get sensor status.
-    void get_sensor_status(SensorStatus& status) const;
+protected:
 
-    // Get sensor status.
-    void get_sensor_configuration(SensorConfiguration& config) const;
+  // Get sensor status.
+  void get_sensor_status(SensorStatus& status) const;
 
-    // Execute SensorCommand.
-    CommandResult execute_sensor_command(const SensorCommand& command);
+  // Get sensor status.
+  void get_sensor_configuration(SensorConfiguration& config) const;
 
-    // Callback for handling command message.
-    virtual void handle_command(const Message& request, Message& response);
+  // Execute SensorCommand.
+  CommandResult execute_sensor_command(const SensorCommand& command);
 
-    // Callback for handling status query message.
-    virtual void handle_status_query(const Message& request, Message& response);
+  // Callback for handling command message.
+  virtual void handle_command(const Message& request, Message& response);
 
-    // Callback for handling configuration query message.
-    virtual void handle_configuration_query(const Message& request, Message& response);
+  // Callback for handling status query message.
+  virtual void handle_status_query(const Message& request, Message& response);
 
-    // Sensor action when activated.
-    virtual void do_activate() = 0;
+  // Callback for handling configuration query message.
+  virtual void handle_configuration_query(const Message& request, Message& response);
 
-    // Sensor action when started.
-    virtual void do_start() = 0;
+  // Sensor action when activated.
+  virtual void do_activate() = 0;
 
-    // Sensor action when activated.
-    virtual void do_stop() = 0;
+  // Sensor action when started.
+  virtual void do_start() = 0;
 
-    // Sensor action when activated.
-    virtual void do_deactivate() = 0;
+  // Sensor action when activated.
+  virtual void do_stop() = 0;
 
-    // Check if rate is supported.
-    virtual bool support_rate(SensorRate rate) = 0;
+  // Sensor action when activated.
+  virtual void do_deactivate() = 0;
+
+  // Check if rate is supported.
+  virtual bool support_rate(SensorRate rate) = 0;
 
 
-  private:
+private:
 
-    CommandResult execute_state_command(StateCommand command);
-    CommandResult execute_rate_command(SensorRate rate);
+  CommandResult execute_state_command(StateCommand command);
+  CommandResult execute_rate_command(SensorRate rate);
 
-    SensorID id_;
-    SensorState state_;
-    SensorRate rate_;
+  SensorID id_;
+  SensorState state_;
+  SensorRate rate_;
 
-    Encoder<SensorCommandResponseCodec> command_response_;
-    Encoder<SensorConfigurationCodec> configuration_;
-    Encoder<SensorStatusCodec> status_;
-  };
+  Encoder<SensorCommandResponseCodec> command_response_;
+  Encoder<SensorConfigurationCodec> configuration_;
+  Encoder<SensorStatusCodec> status_;
+};
 
 } // namespace i3ds
 
