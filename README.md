a654af1c2a9edff04091f433e6c08b92d8205829
Kristoffer Nyborg Gregertsen <kristoffer.gregertsen@sintef.no>
2018-04-27 10:07:28 +0200

    Reorganizes I3DS framework as separate library

--- README.md
+++
@@ -1,4 +0,0 @@
-# I3DS Interface
-
-ASN.1 message defintions for the I3DS sensor suite.
-To be used with ESROCOS and the TASTE framework.
--- scripts/i3ds-format.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ -z "$1" ]
-then
-    echo "Usage: $0 [directory to recusively format C++ files]"
-    exit 1
-fi
-
-exec astyle --style=gnu -s2 -O --recursive  $1/*.cpp $1/*.hpp
