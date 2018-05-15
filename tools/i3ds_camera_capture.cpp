b71c1c0d01d9d3385294f4d45ee46672f4f8077f
Fredrik Bakkevig Haugli <fredrik.haugli@sintef.no>
2018-05-15 16:01:02 +0200

    Enables stereo images in camera-capture

--- tools/i3ds_camera_capture.cpp
+++ tools/i3ds_camera_capture.cpp
@@ -24,6 +24,8 @@ namespace po = boost::program_options;
 typedef i3ds::FrameSensor<i3ds::CameraMeasurement1MCodec>::FrameTopic FrameTopic1M;
 typedef i3ds::FrameSensor<i3ds::CameraMeasurement4MCodec>::FrameTopic FrameTopic4M;
 typedef i3ds::FrameSensor<i3ds::CameraMeasurement8MCodec>::FrameTopic FrameTopic8M;
+typedef i3ds::FrameSensor<i3ds::StereoCameraMeasurement4MCodec>::FrameTopic StereoFrameTopic4M;
+typedef i3ds::FrameSensor<i3ds::StereoCameraMeasurement8MCodec>::FrameTopic StereoFrameTopic8M;
 
 volatile bool running;
 
@@ -34,7 +36,7 @@ signal_handler(int signum)
 }
 
 void
-render_image(unsigned char* image, int rows, int cols, int type)
+render_image(std::string window_name, unsigned char* image, int rows, int cols, int type)
 {
   int cv_type = CV_16UC1;
   if (type == mode_rgb)
@@ -42,9 +44,10 @@ render_image(unsigned char* image, int rows, int cols, int type)
       cv_type = CV_16UC3;
     }
   cv::Mat frame(rows, cols, cv_type, image);
-  cv::imshow("Camera feed", frame);
-  cv::waitKey(100); // Apparently needed to render image properly
+  cv::imshow(window_name, frame);
+  cv::waitKey(5); // Apparently needed to render image properly
 }
+
 
 void
 handle_measurement1M(FrameTopic1M::Data& data)
@@ -52,7 +55,7 @@ handle_measurement1M(FrameTopic1M::Data& data)
   std::cout << "Recv: " << data.attributes.timestamp.microseconds << std::endl;
   int rows = data.region.size_y;
   int cols = data.region.size_x;
-  render_image(static_cast<unsigned char*>(data.image.arr), rows, cols, data.frame_mode);
+  render_image("Camera feed", static_cast<unsigned char*>(data.image.arr), rows, cols, data.frame_mode);
 }
 
 void
@@ -61,7 +64,7 @@ handle_measurement4M(FrameTopic4M::Data& data)
   std::cout << "Recv: " << data.attributes.timestamp.microseconds << std::endl;
   int rows = data.region.size_y;
   int cols = data.region.size_x;
-  render_image(static_cast<unsigned char*>(data.image.arr), rows, cols, data.frame_mode);
+  render_image("Camera feed", static_cast<unsigned char*>(data.image.arr), rows, cols, data.frame_mode);
 }
 
 void
@@ -70,19 +73,41 @@ handle_measurement8M(FrameTopic8M::Data& data)
   std::cout << "Recv: " << data.attributes.timestamp.microseconds << std::endl;
   int rows = data.region.size_y;
   int cols = data.region.size_x;
-  render_image(static_cast<unsigned char*>(data.image.arr), rows, cols, data.frame_mode);
+  render_image("Camera feed", static_cast<unsigned char*>(data.image.arr), rows, cols, data.frame_mode);
+}
+
+void
+handle_stereo_measurement4M(StereoFrameTopic4M::Data& data)
+{
+  std::cout << "Recv: " << data.attributes.timestamp.microseconds << std::endl;
+  int rows = data.region.size_y;
+  int cols = data.region.size_x / 2;
+  render_image("Left camera feed", static_cast<unsigned char*>(data.image_left.arr), rows, cols, data.frame_mode);
+  render_image("Right camera feed", static_cast<unsigned char*>(data.image_right.arr), rows, cols, data.frame_mode);
+}
+
+void
+handle_stereo_measurement8M(StereoFrameTopic8M::Data& data)
+{
+  std::cout << "Recv: " << data.attributes.timestamp.microseconds << std::endl;
+  int rows = data.region.size_y;
+  int cols = data.region.size_x;
+  render_image("Left camera feed", static_cast<unsigned char*>(data.image_left.arr), rows, cols, data.frame_mode);
+  render_image("Right camera feed", static_cast<unsigned char*>(data.image_right.arr), rows, cols, data.frame_mode);
 }
 
 int main(int argc, char *argv[])
 {
   int node;
   int size;
+  bool stereo;
   po::options_description desc("Allowed options");
 
   desc.add_options()
-  ("help", "Produce this message")
-  ("node", po::value<int>(&node)->default_value(10), "Node ID of camera")
-  ("size", po::value<int>(&size)->default_value(1), "Image size: 1, 4 or 8 MiB")
+  ("help,h", "Produce this message")
+  ("node,n", po::value<int>(&node)->default_value(10), "Node ID of camera")
+  ("stereo", po::value<bool>(&stereo)->default_value(false), "capture stereo feed")
+  ("size,s", po::value<int>(&size)->default_value(1), "Image size: 1, 4 or 8 MiB")
   ;
 
   po::variables_map vm;
@@ -95,28 +120,49 @@ int main(int argc, char *argv[])
       return -1;
     }
 
-  cv::namedWindow("Camera feed", cv::WINDOW_AUTOSIZE);
   i3ds::Context::Ptr context = i3ds::Context::Create();
   i3ds::Subscriber subscriber(context);
 
   running = true;
   signal(SIGINT, signal_handler);
 
-  switch(size)
+  if (stereo)
     {
-    case 1:
-      subscriber.Attach<FrameTopic1M>(node, &handle_measurement1M);
-      break;
-    case 4:
-      subscriber.Attach<FrameTopic4M>(node, &handle_measurement4M);
-      break;
-    case 8:
-      subscriber.Attach<FrameTopic8M>(node, &handle_measurement8M);
-      break;
-    default:
-      std::cout << "Invalid image size: " << size;
-      std::cout << ". Available options are: 1, 4 or 8" << std::endl;
-      exit(-1);
+      cv::namedWindow("Left camera feed", cv::WINDOW_AUTOSIZE);
+      cv::namedWindow("Right camera feed", cv::WINDOW_AUTOSIZE);
+      switch(size)
+        {
+        case 4:
+          subscriber.Attach<StereoFrameTopic4M>(node, &handle_stereo_measurement4M);
+          break;
+        case 8:
+          subscriber.Attach<StereoFrameTopic8M>(node, &handle_stereo_measurement8M);
+          break;
+        default:
+          std::cout << "Invalid image size: " << size;
+          std::cout << ". Available options for stereo are: 4 or 8" << std::endl;
+          exit(-1);
+        }
+    }
+  else
+    {
+      cv::namedWindow("Camera feed", cv::WINDOW_AUTOSIZE);
+      switch(size)
+        {
+        case 1:
+          subscriber.Attach<FrameTopic1M>(node, &handle_measurement1M);
+          break;
+        case 4:
+          subscriber.Attach<FrameTopic4M>(node, &handle_measurement4M);
+          break;
+        case 8:
+          subscriber.Attach<FrameTopic8M>(node, &handle_measurement8M);
+          break;
+        default:
+          std::cout << "Invalid image size: " << size;
+          std::cout << ". Available options are: 1, 4 or 8" << std::endl;
+          exit(-1);
+        }
     }
   subscriber.Start();
 
