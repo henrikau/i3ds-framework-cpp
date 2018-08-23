17362bb2d946a0609fbc1335a940b6d72a11c014
Espen Helle <espen.helle@sintef>
2018-08-23 13:49:14 +0200

    Transforms existing code handle and configure wide angular flash

--- tools/i3ds_configure_flash.cpp
+++ tools/i3ds_configure_flash.cpp
@@ -9,11 +9,9 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
-#include <set>
-#include <vector>
 #include <cstdlib>
 
-#include <i3ds/power_client.hpp>
+#include <i3ds/flash_client.hpp>
 
 #include <boost/program_options.hpp>
 
@@ -30,20 +28,17 @@ int main(int argc, char *argv[])
 {
   //i3ds::SensorConfigurator configurator;
   NodeID node_id = 0;
-  std::vector<PowerOutput> enable_ports;
-  std::vector<PowerOutput> disable_ports;
-  bool set_ports = false;
-  std::string addr_server;
+  int strength;
+  int duration;
 
-  po::options_description desc("Allowed trigger control options");
+
+  po::options_description desc("Allowed flash control options");
 
   desc.add_options()
   ("help", "Show help")
-  ("node,n", po::value(&node_id)->default_value(299), "NodeID")
-  ("enable,e", po::value(&enable_ports), "Enable power ports")
-  ("disable,d", po::value(&disable_ports), "Disable power ports")
-  ("set,s", po::bool_switch(&set_ports), "Turn on the enabled ports, turn off all others")
-  ("addr-server,a", po::value(&addr_server)->default_value(""), "Address to the address-server")
+  ("node,n", po::value(&node_id)->default_value(25), "NodeID")
+  ("strength,s", po::value(&strength), "Flash strength (0-100%)")
+  ("duration,d", po::value(&duration), "Duration of flash (1-3000 ms)\n(Be aware, there are some limitations between strength and duration.)")
   ("verbose,v", "Print verbose output") ("quite,q", "Quiet output");
   ;
 
@@ -67,42 +62,13 @@ int main(int argc, char *argv[])
     }
 
   i3ds::Context::Ptr context;
-  if (addr_server.empty()) {
-    context = i3ds::Context::Create();
-  } else {
-    context = std::make_shared<i3ds::Context>(addr_server);
-  }
 
-
-  BOOST_LOG_TRIVIAL(info) << "Connecting to power server with node ID: " << node_id;
-  i3ds::PowerClient power_client(context, node_id);
+  context = i3ds::Context::Create();
+  BOOST_LOG_TRIVIAL(info) << "Connecting to Wide Angular Flash Server with node ID: " << node_id;
+  i3ds::FlashClient flash_client(context, node_id);
   BOOST_LOG_TRIVIAL(trace) << "---> [OK]";
 
-  if (set_ports) {
-    BOOST_LOG_TRIVIAL(info) << "Setting power switches:";
-    for (size_t i=0; i<enable_ports.size(); i++) {
-      BOOST_LOG_TRIVIAL(info) << "  " << enable_ports[i];
-    }
-
-    power_client.set_channels(i3ds::PowerOutputSet(enable_ports.begin(), enable_ports.end()));
-    return 0;
-  }
-
-  if (enable_ports.size() > 0) {
-    BOOST_LOG_TRIVIAL(info) << "Enabling power switches:";
-    for (size_t i=0; i<enable_ports.size(); i++) {
-      BOOST_LOG_TRIVIAL(info) << "  " << enable_ports[i];
-    }
-    power_client.enable_channels(i3ds::PowerOutputSet(enable_ports.begin(), enable_ports.end()));
-  }
-  if (disable_ports.size() > 0) {
-    BOOST_LOG_TRIVIAL(info) << "Disabling power switches:";
-    for (size_t i=0; i<disable_ports.size(); i++) {
-      BOOST_LOG_TRIVIAL(info) << "  " << disable_ports[i];
-    }
-    power_client.disable_channels(i3ds::PowerOutputSet(disable_ports.begin(), disable_ports.end()));
-  }
-
+  flash_client.set_flash(duration, strength);
 
   return 0;
 }
