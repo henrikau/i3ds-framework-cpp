9167bc4786469652f3a87778c14282a6a473572f
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2018-09-05 11:05:57 +0200

    Added scaling of images and file storage

--- tools/i3ds_camera_capture.cpp
+++ tools/i3ds_camera_capture.cpp
@@ -21,6 +21,7 @@
 #include <boost/program_options.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/version.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <chrono>
 #include <iomanip>
@@ -30,8 +31,13 @@ namespace po = boost::program_options;
 volatile bool running;
 
 std::chrono::time_point<std::chrono::high_resolution_clock>previous_time;
+float scale = 100;
+int width = -1;
+bool headless_mode = false;
+std::string output = "", format;
+bool do_output = false, do_scale = false, do_size = false;
 
-
+unsigned int img_index = 0;
 
 void
 signal_handler(int signum)
@@ -40,13 +46,37 @@ signal_handler(int signum)
 }
 
 void
-render_image(std::string window_name, const i3ds::Frame& frame, int image_number, std::string fps_text)
+handle_image(std::string window_name, const i3ds::Frame& frame, int image_number, std::string fps_text)
 {
   cv::Mat mat = frame_to_cv_mat(frame, image_number);
+
+  std::ostringstream path;
+  path <<  output << "_";
+  path << std::setw(5) << std::setfill('0') << img_index;
+  path << "_" << image_number << "." << format;
+
+  cv::imwrite(path.str(), mat);
+  if (headless_mode) {
+    return;
+  }
+
+  cv::Mat outmat;
+
+  if(do_size){
+    cv::Size size = mat.size();
+    float height = (size.height * width)/size.width;
+    cv::resize(mat, outmat, cv::Size(width, height), 0, 0, cv::INTER_AREA);
+  } else if (do_scale) {
+    cv::resize(mat, outmat, cv::Size(0, 0), scale/100, scale/100, cv::INTER_AREA);
+  } else {
+    outmat = mat;
+  }
+
 #if CV_MAJOR_VERSION == 3
   cv::setWindowTitle (window_name, window_name + " " + fps_text);
 #endif
-  cv::imshow(window_name, mat);
+  
+  cv::imshow(window_name, outmat);
   cv::waitKey(5); // Apparently needed to render image properly
 }
 
@@ -61,17 +91,18 @@ handle_frame(i3ds::Camera::FrameTopic::Data& data)
   std::cout << "fps: "<< buffer.str();
 
   previous_time = time_now;
+  img_index++;
 
 
   switch (data.descriptor.image_count)
     {
     case 1:
-      render_image("Camera feed", data, 0, buffer.str());
+      handle_image("Camera feed", data, 0, buffer.str());
       break;
 
     case 2:
-      render_image("Left camera feed", data, 0, buffer.str());
-      render_image("Right camera feed", data, 1, buffer.str());
+      handle_image("Left camera feed", data, 0, buffer.str());
+      handle_image("Right camera feed", data, 1, buffer.str());
       break;
 
     default:
@@ -82,13 +113,16 @@ handle_frame(i3ds::Camera::FrameTopic::Data& data)
 int main(int argc, char *argv[])
 {
   int node;
-  bool stereo;
   po::options_description desc("Displays mono or stereo video stream\n  Available options");
 
   desc.add_options()
   ("help,h", "Produce this message")
   ("node,n", po::value<int>(&node)->default_value(10), "Node ID of camera")
-  ("stereo", po::value<bool>(&stereo)->default_value(false), "capture stereo feed")
+  ("scale,x", po::value(&scale), "Camera scale [%]")
+  ("width,w", po::value(&width), "Maximal image width [px]") 
+  ("output,o", po::value(&output), "Output path template") 
+  ("format,f", po::value(&format)->default_value("tiff"), "Output format") 
+  ("nogui,g", po::bool_switch(&headless_mode), "Headless mode") 
   ;
 
   po::variables_map vm;
@@ -101,21 +135,24 @@ int main(int argc, char *argv[])
       return -1;
     }
 
+  if (vm.count("width")) {
+    do_size = true;
+    std::cout << "Scaling width to: " << width << " [px]" << std::endl;
+  } else if (vm.count("scale")) {
+    do_scale = true;
+    std::cout << "Scale: " << scale << "%" << std::endl;
+  }
+
+  if (vm.count("output")) {
+    do_output = true;
+    std::cout << "Storing data to: " << output << "_XXXXX." << format << std::endl;
+  }
+
   i3ds::Context::Ptr context = i3ds::Context::Create();
   i3ds::Subscriber subscriber(context);
 
   running = true;
   signal(SIGINT, signal_handler);
-
-  if (stereo)
-    {
-      cv::namedWindow("Left camera feed", cv::WINDOW_AUTOSIZE);
-      cv::namedWindow("Right camera feed", cv::WINDOW_AUTOSIZE);
-    }
-  else
-    {
-      cv::namedWindow("Camera feed", cv::WINDOW_AUTOSIZE);
-    }
 
   subscriber.Attach<i3ds::Camera::FrameTopic>(node, &handle_frame);
   subscriber.Start();
