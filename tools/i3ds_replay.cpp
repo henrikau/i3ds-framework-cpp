b17b5aa044fc386ff6e9384ca94c6d06245f0dbc
Kristoffer Nyborg Gregertsen <kristoffer.gregertsen@sintef.no>
2018-11-23 14:02:06 +0100

    Adds speedup to replay and fixes style

--- tools/i3ds_replay.cpp
+++ tools/i3ds_replay.cpp
@@ -39,13 +39,19 @@ signal_handler(int signum)
 
 bool
 register_node(i3ds::Context::Ptr &context,
-	      std::unordered_map<NodeID, i3ds::Socket::Ptr> &publishers,
-	      NodeID new_node) {
+              std::unordered_map<NodeID, i3ds::Socket::Ptr> &publishers,
+              NodeID new_node)
+{
   auto search = publishers.find(new_node);
+
   if (search != publishers.end())
-    return false;
+    {
+      return false;
+    }
+
   publishers[new_node] = i3ds::Socket::Publisher(context);
   publishers[new_node]->Attach(new_node);
+
   return true;
 }
 
@@ -59,6 +65,7 @@ main(int argc, char *argv[])
   int node_offset;
   bool is_forced_node = false;
   NodeID forced_node;
+  double speedup;
 
   desc.add_options()
   ("help,h", "Produce this message")
@@ -66,6 +73,7 @@ main(int argc, char *argv[])
   ("verbose,v", "Print verbose output")
   ("node,n", po::value<NodeID>(&forced_node), "Force the messages to be output on this node")
   ("node-offset,p", po::value<int>(&node_offset)->default_value(0), "Offset to the recorded nodes")
+  ("speedup,s", po::value<double>(&speedup)->default_value(1.0), "Speedup of replay (1.0 = normal speed)")
   ("quiet,q", "Quiet ouput")
   ;
 
@@ -101,59 +109,87 @@ main(int argc, char *argv[])
   i3ds::MessageRecord r = recording.get_message();
 
   NodeID node_id;
-  if (recording.header_found()) {
-    BOOST_LOG_TRIVIAL(trace) << "Header file found.";
-    BOOST_LOG_TRIVIAL(info) << "Replaying messages from node IDs:";
-    for (NodeID node_id : recording.node_ids) {
-      BOOST_LOG_TRIVIAL(info) << "  " << node_id;
+
+  if (recording.header_found())
+    {
+      BOOST_LOG_TRIVIAL(trace) << "Header file found.";
+      BOOST_LOG_TRIVIAL(info) << "Replaying messages from node IDs:";
+
+      for (NodeID node_id : recording.node_ids)
+        {
+          BOOST_LOG_TRIVIAL(info) << "  " << node_id;
+        }
     }
-  } else {
-    BOOST_LOG_TRIVIAL(trace) << "Header file not found.";
-    node_id = r.node();
-    BOOST_LOG_TRIVIAL(info) << "Replaying messages from node ID " <<  node_id << "(and possibly others)";
-  }
+  else
+    {
+      BOOST_LOG_TRIVIAL(trace) << "Header file not found.";
+      node_id = r.node();
+      BOOST_LOG_TRIVIAL(info) << "Replaying messages from node ID " <<  node_id << "(and possibly others)";
+    }
 
-  if (recording.has_endpoint_id) {
-    BOOST_LOG_TRIVIAL(info) << " with endpoint ID " << recording.endpoint_id;
-  }
+  if (recording.has_endpoint_id)
+    {
+      BOOST_LOG_TRIVIAL(info) << " with endpoint ID " << recording.endpoint_id;
+    }
 
-  if (vm.count("node")) {
-    is_forced_node = true;
-  }
+  if (vm.count("node"))
+    {
+      is_forced_node = true;
+    }
 
   std::unordered_map<NodeID, NodeID> node_id_translation;
 
-  if (recording.header_found()) {
-    if (is_forced_node) {
-      BOOST_LOG_TRIVIAL(info) << "Publishing to node ID " << forced_node;
-      for (NodeID node_id : recording.node_ids) {
-	node_id_translation[node_id] = forced_node;
-      }
-    } else {
-      BOOST_LOG_TRIVIAL(info) << "Publishing to node IDs: ";
-      for (NodeID node_id : recording.node_ids) {
-	NodeID new_node = node_id + node_offset;
-	node_id_translation[node_id] = new_node;
-	BOOST_LOG_TRIVIAL(info) << "  " << new_node;
-      }
+  if (recording.header_found())
+    {
+      if (is_forced_node)
+        {
+          BOOST_LOG_TRIVIAL(info) << "Publishing to node ID " << forced_node;
+
+          for (NodeID node_id : recording.node_ids)
+            {
+              node_id_translation[node_id] = forced_node;
+            }
+        }
+      else
+        {
+          BOOST_LOG_TRIVIAL(info) << "Publishing to node IDs: ";
+
+          for (NodeID node_id : recording.node_ids)
+            {
+              NodeID new_node = node_id + node_offset;
+              node_id_translation[node_id] = new_node;
+              BOOST_LOG_TRIVIAL(info) << "  " << new_node;
+            }
+        }
     }
-  } else {
-    if (is_forced_node) {
-      BOOST_LOG_TRIVIAL(info) << "Publishing to node ID " <<  forced_node;
-      node_id_translation[node_id] = forced_node;
-    } else {
-      NodeID new_node = node_id + node_offset;
-      BOOST_LOG_TRIVIAL(info) << "Publishing to node ID " << new_node << "(and possibly others)";
-      node_id_translation[node_id] = new_node;
+  else
+    {
+      if (is_forced_node)
+        {
+          BOOST_LOG_TRIVIAL(info) << "Publishing to node ID " <<  forced_node;
+          node_id_translation[node_id] = forced_node;
+        }
+      else
+        {
+          NodeID new_node = node_id + node_offset;
+          BOOST_LOG_TRIVIAL(info) << "Publishing to node ID " << new_node << "(and possibly others)";
+          node_id_translation[node_id] = new_node;
+        }
     }
-  }
 
   i3ds::Context::Ptr context = i3ds::Context::Create();
   std::unordered_map<NodeID, i3ds::Socket::Ptr> publishers;
 
-  for (auto node_node : node_id_translation) {
-    register_node(context, publishers, node_node.second);
-  }
+  for (auto node_node : node_id_translation)
+    {
+      register_node(context, publishers, node_node.second);
+    }
+
+  if (speedup < 0.001)
+    {
+      BOOST_LOG_TRIVIAL(error) << "Setting speedup to 0.001, was " << speedup;
+      speedup = 0.001;
+    }
 
   running = true;
   signal(SIGINT, signal_handler);
@@ -165,51 +201,68 @@ main(int argc, char *argv[])
   std::this_thread::sleep_for(std::chrono::milliseconds(300));
   BOOST_LOG_TRIVIAL(trace) << "Done.";
 
-  try {
-
-    while (running)
+  try
     {
-      n_messages++;
-      if (prev_time < r.timestamp) {
-	uint64_t delay = r.timestamp - prev_time;
-	std::this_thread::sleep_for(std::chrono::microseconds(delay));
-      }
+      while (running)
+        {
+          n_messages++;
 
-      i3ds::Address addr = r.msg->address();
-      NodeID old_node = addr.node;
-      NodeID new_node;
-      auto search = node_id_translation.find(old_node);
-      if (search == node_id_translation.end()) {
-	// This is the first time we see this ID.
-	if (is_forced_node) {
-	  new_node = forced_node;
-	} else {
-	  new_node = addr.node + node_offset;
-	}
-        node_id_translation[addr.node] = new_node;
-	if (register_node(context, publishers, new_node)) {
-	  // This should only be executed if header is missing
-	  std::this_thread::sleep_for(std::chrono::milliseconds(300));
-	}
-      } else {
-        new_node = search->second;
-      }
+          if (prev_time < r.timestamp)
+            {
+              uint64_t delay = r.timestamp - prev_time;
+              std::this_thread::sleep_for(std::chrono::microseconds(static_cast<uint64_t>(delay / speedup)));
+            }
 
-      if (new_node != old_node) {
-	BOOST_LOG_TRIVIAL(trace) << "Changing address from " << addr.node << ":" << addr.endpoint;
-	addr.node = new_node;
-	BOOST_LOG_TRIVIAL(trace) << "                 to   " << addr.node << ":" << addr.endpoint;
-	r.msg->set_address(addr);
-      }
+          i3ds::Address addr = r.msg->address();
+          NodeID old_node = addr.node;
+          NodeID new_node;
 
-      publishers[new_node]->Send(*(r.msg));
-      BOOST_LOG_TRIVIAL(trace) << "Sent message";
-      prev_time = r.timestamp;
+          auto search = node_id_translation.find(old_node);
 
-      r = recording.get_message();
+          if (search == node_id_translation.end())
+            {
+              // This is the first time we see this ID.
+              if (is_forced_node)
+                {
+                  new_node = forced_node;
+                }
+              else
+                {
+                  new_node = addr.node + node_offset;
+                }
+
+              node_id_translation[addr.node] = new_node;
+
+              if (register_node(context, publishers, new_node))
+                {
+                  // This should only be executed if header is missing
+                  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+                }
+            }
+          else
+            {
+              new_node = search->second;
+            }
+
+          if (new_node != old_node)
+            {
+              BOOST_LOG_TRIVIAL(trace) << "Changing address from " << addr.node << ":" << addr.endpoint;
+              addr.node = new_node;
+              BOOST_LOG_TRIVIAL(trace) << "                 to   " << addr.node << ":" << addr.endpoint;
+              r.msg->set_address(addr);
+            }
+
+          publishers[new_node]->Send(*(r.msg));
+          BOOST_LOG_TRIVIAL(trace) << "Sent message";
+          prev_time = r.timestamp;
+
+          r = recording.get_message();
+        }
     }
-  } catch (i3ds::exceptions::end_of_file &e) {
-  }
+  catch (i3ds::exceptions::end_of_file &e)
+    {
+    }
+
   BOOST_LOG_TRIVIAL(info) << "Played back " << n_messages << " messages.";
 
   return 0;
