138b89dd29b10f7b207d614d6aa55e50c9b7f253
Fredrik Bakkevig Haugli <fredrik.haugli@sintef.no>
2019-11-18 15:28:08 +0100

    Fixes copyright year in pose_estimator_sensor.cpp

--- libi3ds/src/pose_estimator_sensor.cpp
+++ libi3ds/src/pose_estimator_sensor.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////\file
 ///
-///   Copyright 2018 SINTEF AS
+///   Copyright 2019 SINTEF AS
 ///
 ///   This Source Code Form is subject to the terms of the Mozilla
 ///   Public License, v. 2.0. If a copy of the MPL was not distributed
